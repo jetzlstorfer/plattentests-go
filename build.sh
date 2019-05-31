6191574025486ffbe292815f369a2f3f1bf1d0b2
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2019-05-31 09:44:40 +0200

    include build for linux

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@
 # MAC
 env GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=0.0.1 -X main.date=`date -u +%Y%m%d-%H:%M`" -o ./bin/plattentestsgo
 
+# Linux
+env GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=0.0.1 -X main.date=`date -u +%Y%m%d-%H:%M`" -o ./bin/plattentestsgo-linux
+
 # Windows build 
 env GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=0.0.1 -X main.date=`date -u +%Y%m%d-%H:%M`" -o ./bin/plattentestsgo.exe
