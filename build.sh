e0ea6e8d9a875592ff0437efcecd2c6fae89b96e
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2019-05-31 09:43:01 +0200

    build script for mac os and windows

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#VERSION=$(cat version | tr -d '[:space:]')
+#go build -ldflags="-X 'main.Version=$VERSION'"  -o keptn
+
+# MAC
+env GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=0.0.1 -X main.date=`date -u +%Y%m%d-%H:%M`" -o ./bin/plattentestsgo
+
+# Windows build 
+env GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=0.0.1 -X main.date=`date -u +%Y%m%d-%H:%M`" -o ./bin/plattentestsgo.exe
