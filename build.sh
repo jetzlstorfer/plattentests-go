b8c755d4755da1b00f0583d4cfda8d8d0c54f2ce
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2019-05-31 09:45:56 +0200

    include linux build

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-#VERSION=$(cat version | tr -d '[:space:]')
-#go build -ldflags="-X 'main.Version=$VERSION'"  -o keptn
-
-# MAC
+# Mac OS
 env GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=0.0.1 -X main.date=`date -u +%Y%m%d-%H:%M`" -o ./bin/plattentestsgo
 
 # Linux
 env GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=0.0.1 -X main.date=`date -u +%Y%m%d-%H:%M`" -o ./bin/plattentestsgo-linux
 
-# Windows build 
+# Windows  
 env GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=0.0.1 -X main.date=`date -u +%Y%m%d-%H:%M`" -o ./bin/plattentestsgo.exe
