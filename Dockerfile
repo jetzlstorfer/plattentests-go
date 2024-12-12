15919291b2b6eef939375f7ae0e0c7c68e6a403d
Jürgen Etzlstorfer <juergen.etzlstorfer@gmail.com>
2024-12-12 07:39:08 +0000

    update dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.20-alpine as BuildStage
+FROM golang:1.23-alpine as BuildStage
 
 WORKDIR /app
 
--- main.go
+++ main.go
@@ -1,7 +1,8 @@
-//  1. Register an application at: https://developer.spotify.com/my-applications/
-//     - Use "http://localhost:8080/callback" as the redirect URI
-//  2. Set the SPOTIFY_ID environment variable to the client ID you got in step 1.
-//  3. Set the SPOTIFY_SECRET environment variable to the client secret from step 1.
+// 1. Register an application at: https://developer.spotify.com/my-applications/
+//   - Use "http://localhost:8080/callback" as the redirect URI
+//
+// 2. Set the SPOTIFY_ID environment variable to the client ID you got in step 1.
+// 3. Set the SPOTIFY_SECRET environment variable to the client secret from step 1.
 package main
 
 import (
@@ -305,7 +306,6 @@ func sanitizeTrackname(trackname string) string {
 	return sanitizedName
 }
 
-// remove duplicates from array
 func removeDuplicates(sliceList []spotify.ID) []spotify.ID {
 	allKeys := make(map[spotify.ID]bool)
 	list := []spotify.ID{}
